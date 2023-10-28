4a064df945659b4d8f8a54fde992f9bdd15a4b2b
ikhushisingla <singlakhushi822@gmail.com>
2023-10-29 03:50:58 +0530

    authentication error resolved

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () => {
-  const {isAuthenticated,isLoading}=useConvexAuth
+  const {isAuthenticated,isLoading}=useConvexAuth()
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
