fece4d56df29bec2c6c96c687bfc4580b73ea7fd
ikhushisingla <singlakhushi822@gmail.com>
2023-10-31 11:22:08 +0530

    search functionality added

--- app/(main)/_components/modals/confirm-modal.tsx
+++ app/(main)/_components/modals/confirm-modal.tsx
@@ -1,44 +1,56 @@
-"use client"
+"use client";
 
-import { AlertDialog,AlertDialogAction,AlertDialogCancel,AlertDialogContent,AlertDialogDescription,AlertDialogFooter,AlertDialogHeader,AlertDialogTitle,AlertDialogTrigger } from "@/components/ui/alert-dialog"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "@/components/ui/alert-dialog";
 
-interface ConfirmModalProps{
-    children:React.ReactNode;
-    onConfirm:()=>void;
-}
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+};
 
-export const ConfirmModal=({
-    children,
-    onConfirm
-}:ConfirmModalProps)=>{
+export const ConfirmModal = ({
+  children,
+  onConfirm
+}: ConfirmModalProps) => {
+  const handleConfirm = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    onConfirm();
+  };
 
-    const handleConfirm=(e:React.MouseEvent<HTMLDivElement,MouseEvent>)=>{
-        e.stopPropagation();
-        onConfirm();
-    }
-    return(
-        <AlertDialog>
-            <AlertDialogTrigger onClick={(e)=>e.stopPropagation()} asChild>
-                {children}
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>
-                        Are you sure you want to delete this?
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                        This action cannot be undone!
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel onClick={(e)=>e.stopPropagation()}>
-                        Cancel
-                    </AlertDialogCancel>
-                    <AlertDialogAction onClick={handleConfirm}>
-                        Confirm
-                    </AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    )
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger onClick={(e) => e.stopPropagation()} asChild>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you absolutely sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={e => e.stopPropagation()}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirm}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
 }
