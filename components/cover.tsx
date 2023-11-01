9728669933c0d4679a9b8d80467b120b19b8d588
ikhushisingla <singlakhushi822@gmail.com>
2023-11-01 14:15:25 +0530

    advance file upload

--- components/cover.tsx
+++ components/cover.tsx
@@ -9,6 +9,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface coverImageProps{
     url?:string;
@@ -19,10 +20,16 @@ export const Cover=({
     url,
     preview
 }:coverImageProps)=>{
+    const {edgestore}=useEdgeStore()
     const params=useParams()
     const coverImage=useCoverImage()
     const removeCoverImage=useMutation(api.documents.removeCoverImage)
     const onRemove=()=>{
+        if(url){
+            edgestore.publicFiles.delete({
+                url:url
+            })
+        }
         removeCoverImage({
             id:params.documentId as Id<"documents">
         })
@@ -35,7 +42,7 @@ export const Cover=({
 
             {url && !preview && (
                 <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
-                    <Button onClick={coverImage.onOpen} className="text-muted-foreground text-xs"variant="outline" size="sm">
+                    <Button onClick={()=>coverImage.onReplace(url)} className="text-muted-foreground text-xs"variant="outline" size="sm">
                         <ImageIcon className="h-4 w-4 mr-2"/>
                             Change cover
                     </Button>
