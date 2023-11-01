929b77274edc820f9cba8fd62e9e332c45650570
ikhushisingla <singlakhushi822@gmail.com>
2023-11-01 18:28:25 +0530

    Editor and error page done

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,49 +1,56 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
-import { Button } from "@/components/ui/button";
-import { PlusCircleIcon } from "lucide-react";
+import { PlusCircle } from "lucide-react";
 import { useMutation } from "convex/react";
-import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-const DocumentPage = () => {
-    const {user} =useUser();
-    const create=useMutation(api.documents.create);
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
-    const onCreate=()=>{
-      const promise=create({title:"Untitled"})
+const DocumentsPage = () => {
+  const router = useRouter();
+  const { user } = useUser();
+  const create = useMutation(api.documents.create);
 
-      toast.promise(promise,{
-         loading:"Creating a new note...",
-         success:"New note created!",
-         error:"Failed to create a new note"
-      });
-    }
-    return ( 
-        <div className="h-full flex flex-col items-center justify-center space-y-4">
-           <Image
-            src="/empty.png"
-            height="300"
-            width="300"
-            alt="Empty"
-            className="dark:hidden"
-           />
-           <Image
-            src="/empty-dark.png"
-            height="300"
-            width="300"
-            alt="Empty"
-            className="hidden dark:block"
-           />
-           <h2 className="text-lg font-medium">Welcome to {user?.firstName}&apos;s Studyio</h2>
-           <Button onClick={onCreate}>
-            <PlusCircleIcon className="h-4 w-4 mr-2"/>
-            Create a note!
-           </Button>
-        </div>
-     );
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" })
+      .then((documentId) => router.push(`/documents/${documentId}`))
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note."
+    });
+  };
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Studyio
+      </h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+   );
 }
  
-export default DocumentPage;
+export default DocumentsPage;
